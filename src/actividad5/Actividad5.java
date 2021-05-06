9c3a2930b106c368c6c37a587f9777e55b64d1fd
cj_be <cj_be@DESKTOP-7ENKCBI>
2021-05-05 23:26:56 -0500

    1.- Imprima un arreglo que tenga los nombres de los días de la semana. 2.- Imprima utilizando los ciclos for el arreglo de dos dimensiones de los personajes de Star War. 3.- Dado el arreglo de dos dimensiones del alfabeto y su correspondencia en código morse, imprima la letra y su código morse asignado. 4.- Lea una palabra, conviertala a unarreglo de caracteres y busque su correspondencia en el código ascii. 5.-Con su algoritmo de impresión del código morse, lea una frase y contabilice las palabras si son menos de 5 cobrar 10 pesos y por cada palabra extra cobrar 3 pesos, calcule e imprima el total a pagar y la frase en código morse. 6.- Lea una frase que identifique el sexo de los personajes de StarWar y realice un filtro, para ello utilice un ciclo que imprima el sexo de los personajes seleccionado  [male, female, n/a, hermaphrodite]

---
+++ src/actividad5/Actividad5.java
@@ -0,0 +1,342 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad5;
+import  java.util.Scanner ;
+
+/**
+ *
+ * @author Ehuan Cisneros Monica de los Angeles
+ */
+public class Actividad5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     Scanner leer = new Scanner(System.in);
+                 System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[Pirmer ejercicio]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");  
+        System.out.println(""); 
+            String diasSem[] = new String [7];
+        diasSem = new String[7];
+        diasSem [0] = "Lunes";
+        diasSem [1] = "Martes";
+        diasSem [2] = "Miercoles";
+        diasSem [3] = "Jueves";
+        diasSem [4] = "Viernes";
+        diasSem [5] = "sabado";
+        diasSem [6] = "domingo";
+    
+    
+              System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[Segundo ejercicio]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");  
+        System.out.println(""); 
+  System.out.println("Matriz de personajes");
+        String sWar[][] = new String[3][4];
+        sWar[0][0]="Darth Vader";
+        sWar[0][1]="Luke Skywalker";
+        sWar[0][2]="Han solo";
+        sWar[0][3]="Lea Organa";
+        sWar[1][0]="Obi-Wan Kenobi";
+        sWar[1][1]="Chewbacca";
+        sWar[1][2]="Yoda";
+        sWar[1][3]="Palpatine";
+        sWar[2][0]="R2-D2";
+        sWar[2][1]="C-3PO";
+        sWar[2][2]="Boba Fett";
+        sWar[2][3]="Darth Maul";
+        int i,j;
+        for (i = 0;i < sWar.length; i++){
+            for(j = 0; j < sWar[i].length; j++){
+                System.out.print("["+sWar[i][j]+"]");
+                
+            }
+        }
+        System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[tercero ejercicio]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");  
+        System.out.println("");
+         String aCodigo[][]  = new String[26][2];
+        
+        aCodigo[0][0] = "A";
+        aCodigo[1][0] = "B";
+        aCodigo[2][0] = "C";
+        aCodigo[3][0] = "D";
+        aCodigo[4][0] = "E";
+        aCodigo[5][0] = "F";
+        aCodigo[6][0] = "G";
+        aCodigo[7][0] = "H";
+        aCodigo[8][0] = "I";
+        aCodigo[9][0] = "J";
+        aCodigo[10][0] = "K";
+        aCodigo[11][0] = "L";
+        aCodigo[12][0] = "M";
+        aCodigo[13][0] = "N";
+        aCodigo[14][0] = "O";
+        aCodigo[15][0] = "P";
+        aCodigo[16][0] = "Q";
+        aCodigo[17][0] = "R";
+        aCodigo[18][0] = "S";
+        aCodigo[19][0] = "T";
+        aCodigo[20][0] = "U";
+        aCodigo[21][0] = "V";
+        aCodigo[22][0] = "W";
+        aCodigo[23][0] = "X";
+        aCodigo[24][0] = "Y";
+        aCodigo[25][0] = "Z";
+    
+        aCodigo[0][1] = ".-";
+        aCodigo[1][1] = "-...";
+        aCodigo[2][1] = "-.-.";
+        aCodigo[3][1] = "-..";
+        aCodigo[4][1] = ".";
+        aCodigo[5][1] = "..-.";
+        aCodigo[6][1] = "--.";
+        aCodigo[7][1] = "...";
+        aCodigo[8][1] = "..";
+        aCodigo[9][1] = ".---";
+        aCodigo[10][1] = "-.-";
+        aCodigo[11][1] = ".-..";
+        aCodigo[12][1] = "--";
+        aCodigo[13][1] = "-.";
+        aCodigo[14][1] = "---";
+        aCodigo[15][1] = ".--.";
+        aCodigo[16][1] = "--.-";
+        aCodigo[17][1] = ".-.";
+        aCodigo[18][1] = "...";
+        aCodigo[19][1] = "-";
+        aCodigo[20][1] = "..-";
+        aCodigo[21][1] = "...-";
+        aCodigo[22][1] = ".--";
+        aCodigo[23][1] = "-..-";
+        aCodigo[24][1] = "-.--";
+        aCodigo[25][1] = "--..";
+        
+                     for (String[] aCodigo1 : aCodigo) {
+                         System.out.println(aCodigo1[0] + " : " + aCodigo1[1]);
+                     }
+        
+ System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[cuarto ejercicio]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");  
+        System.out.println("");
+        
+  System.out.println("Ingrese una palabra sin espacios:");
+       
+        String palabra = leer.next();
+        char [] aChar = palabra.toCharArray();
+        for (int i = 0; i < aChar.length; i++) {
+            
+            System.out.println(aChar[i]);
+
+        }
+
+  System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[quinto ejercicio]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");  
+        System.out.println("");
+        
+     String aCodigo[][]  = new String[26][2];
+        
+        aCodigo[0][0] = "A";
+        aCodigo[1][0] = "B";
+        aCodigo[2][0] = "C";
+        aCodigo[3][0] = "D";
+        aCodigo[4][0] = "E";
+        aCodigo[5][0] = "F";
+        aCodigo[6][0] = "G";
+        aCodigo[7][0] = "H";
+        aCodigo[8][0] = "I";
+        aCodigo[9][0] = "J";
+        aCodigo[10][0] = "K";
+        aCodigo[11][0] = "L";
+        aCodigo[12][0] = "M";
+        aCodigo[13][0] = "N";
+        aCodigo[14][0] = "O";
+        aCodigo[15][0] = "P";
+        aCodigo[16][0] = "Q";
+        aCodigo[17][0] = "R";
+        aCodigo[18][0] = "S";
+        aCodigo[19][0] = "T";
+        aCodigo[20][0] = "U";
+        aCodigo[21][0] = "V";
+        aCodigo[22][0] = "W";
+        aCodigo[23][0] = "X";
+        aCodigo[24][0] = "Y";
+        aCodigo[25][0] = "Z";
+    
+        aCodigo[0][1] = ".-";
+        aCodigo[1][1] = "-...";
+        aCodigo[2][1] = "-.-.";
+        aCodigo[3][1] = "-..";
+        aCodigo[4][1] = ".";
+        aCodigo[5][1] = "..-.";
+        aCodigo[6][1] = "--.";
+        aCodigo[7][1] = "...";
+        aCodigo[8][1] = "..";
+        aCodigo[9][1] = ".---";
+        aCodigo[10][1] = "-.-";
+        aCodigo[11][1] = ".-..";
+        aCodigo[12][1] = "--";
+        aCodigo[13][1] = "-.";
+        aCodigo[14][1] = "---";
+        aCodigo[15][1] = ".--.";
+        aCodigo[16][1] = "--.-";
+        aCodigo[17][1] = ".-.";
+        aCodigo[18][1] = "...";
+        aCodigo[19][1] = "-";
+        aCodigo[20][1] = "..-";
+        aCodigo[21][1] = "...-";
+        aCodigo[22][1] = ".--";
+        aCodigo[23][1] = "-..-";
+        aCodigo[24][1] = "-.--";
+        aCodigo[25][1] = "--..";
+      
+        System.out.println("Escribe una palabra solo en MAYUSCULA:");
+      
+        String palabra = leer.next();
+        int Tpagar = 10;
+        int asciiValue;
+        char cChar;
+        int x=0;
+        char [] aChar = palabra.toCharArray();
+        for (int i = 0; i < aChar.length; i++) {
+            cChar = aChar[i];
+            asciiValue= (int) cChar;
+            x = i;  
+            switch (asciiValue) {
+                case 65: System.out.println("A:"+aCodigo[65-65][1]);
+                    break;
+                case 66: System.out.println("B:"+aCodigo[66-65][1]);
+                    break;
+                case 67: System.out.println("C:"+aCodigo[67-65][1]);
+                    break;
+                case 68: System.out.println("D:"+aCodigo[68-65][1]);
+                    break;
+                case 69: System.out.println("E:"+aCodigo[68-65][1]);
+                    break;
+                case 70: System.out.println("F:"+aCodigo[69-65][1]);
+                    break;
+                case 71: System.out.println("G:"+aCodigo[70-65][1]);
+                    break;
+                case 72: System.out.println("H:"+aCodigo[71-65][1]);
+                    break;
+                case 73: System.out.println("I:"+aCodigo[72-65][1]);
+                    break;
+                case 74: System.out.println("J:"+aCodigo[73-65][1]);
+                    break;
+                case 75: System.out.println("K:"+aCodigo[74-65][1]);
+                    break;
+                case 76: System.out.println("L:"+aCodigo[75-65][1]);
+                     break;
+                case 77: System.out.println("M:"+aCodigo[76-65][1]);
+                     break;
+                case 78: System.out.println("N:"+aCodigo[77-65][1]);
+                     break;
+                case 79: System.out.println("O:"+aCodigo[78-65][1]);
+                    break;
+                case 80: System.out.println("P:"+aCodigo[79-65][1]);
+                    break;
+                case 81: System.out.println("Q:"+aCodigo[80-65][1]);
+                    break;
+                case 82: System.out.println("R:"+aCodigo[81-65][1]);
+                    break;
+                case 83: System.out.println("S:"+aCodigo[82-65][1]);
+                    break;
+                case 84: System.out.println("T:"+aCodigo[83-65][1]);
+                    break;
+                case 85: System.out.println("U:"+aCodigo[84-65][1]);
+                    break;
+                case 86:  System.out.println("V:"+aCodigo[85-65][1]);
+                    break;
+                case 87: System.out.println("W:"+aCodigo[86-65][1]);
+                    break;
+                case 88: System.out.println("X:"+aCodigo[87-65][1]);
+                    break;
+                case 89: System.out.println("Y:"+aCodigo[88-65][1]);
+                    break;
+                case 90: System.out.println("Z:"+aCodigo[89-65][1]);
+                    break;
+            }
+        }
+        if (x<=aChar.length) {
+                if (x<=4) {
+                    System.out.println("total a pagar ="+ Tpagar);
+                }
+                else{
+                    int extra = x-4;
+                    for (int j = 0; j < extra; j++) {
+                        Tpagar = Tpagar +3;
+                    }
+                    System.out.println("total a pagar mas el extra ="+ Tpagar);
+                }
+            }
+  System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°[Sexto ejercicio]°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");  
+        System.out.println("");
+ System.out.println("ingresa un sexo:");
+        Scanner leer = new Scanner(System.in);
+        String sWar[][] = new String[3][10];
+        sWar[0][0]="Darth Vader";
+        sWar[1][0]="2,03 m";
+        sWar[2][0]="male";
+        
+        sWar[0][1]="Luke Skywalker";
+        sWar[1][1]="1.72 m";
+        sWar[2][1]="male";
+        
+        sWar[0][2]="Han solo";
+        sWar[1][2]="1.8 m";
+        sWar[2][2]="male";
+        
+        sWar[0][3]="Lea Organa";
+        sWar[1][3]="2,03 m";
+        sWar[2][3]="female";
+        
+        sWar[0][4]="Asoka Tano";
+        sWar[1][4]="1.88 m";
+        sWar[2][4]="female";
+        
+        sWar[0][5]="Rey Skywalker";
+        sWar[1][5]="1.7 m";
+        sWar[2][5]="female";
+        
+        sWar[0][6]="Jabba el Hutt";
+        sWar[1][6]="3,9 m";
+        sWar[2][6]="hermafrodita";
+        
+        sWar[0][7]="Zorba Desilijic";
+        sWar[1][7]="3,03 m";
+        sWar[2][7]="hermafrodita";
+        
+        sWar[0][8]="R2-D2";
+        sWar[1][8]="2,03 m";
+        sWar[2][8]="programming";
+        
+        sWar[0][9]="C-3PO";
+        sWar[1][9]="2,03 m";
+        sWar[2][9]="programming";
+        String nameS = leer.next();
+        switch(nameS){
+            case "male": 
+                for (int i = 0; i < 3; i++) {
+                    System.out.println(sWar[0][i]);
+                }
+                break;
+            case "femele":
+                for (int i = 3; i < 5; i++) {
+                    System.out.println(sWar[0][i]);
+                }
+                break;
+            case "hermafrodita":
+                for (int i = 5; i < 7; i++) {
+                    System.out.println(sWar[0][i]);
+                }
+                break;
+            case "programming":
+                for (int i = 7; i < 9; i++) {
+                    System.out.println(sWar[0][i]);
+                }
+                break;
+            
+        }
+    }
+}
+
+        
+
